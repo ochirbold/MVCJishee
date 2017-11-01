544906eda3ef92491819d560940c4c8241ca62a6
Ochirbold Manal <chogoroo@yahoo.com>
2017-11-01 17:29:35 +0800

    Өгөгдлийн сан үүсгээд CRUD хийж болдог жишээ байна. хийгээд үзсэний дараа

---
+++ WebApplication3/WebApplication3/Controllers/CRUDController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class CRUDController : Controller
+    {
+        DFContext db = new DFContext();
+        public ActionResult Index()
+        {
+
+            return View(db.EmpUsers.ToList());
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(EmpUser empUser)
+        {
+            if (ModelState.IsValid)
+                {
+                db.EmpUsers.Add(empUser);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+                }
+            return View();
+        }
+    }
+}
